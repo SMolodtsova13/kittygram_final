3072bb3201c9714093574c8886a20228a410b9c4
Svetlana Molodtsova <167689948+SMolodtsova13@users.noreply.github.com>
2024-08-29 15:50:30 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -94,6 +94,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+# Указываем корневую директорию для сборки статических файлов;
+# в контейнере это будет /app/collected_static
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
