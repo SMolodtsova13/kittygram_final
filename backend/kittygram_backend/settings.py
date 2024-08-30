2c59b3710b54bb0c2b114f3f11f31232e1a6da27
Svetlana Molodtsova <167689948+SMolodtsova13@users.noreply.github.com>
2024-08-30 22:12:02 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,21 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
+
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 if not SECRET_KEY:
     SECRET_KEY = get_random_secret_key()
-
-DEBUG = True
-
-ALLOWED_HOSTS = ['89.169.172.126', 'smolod-kittygram.webhop.me', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
